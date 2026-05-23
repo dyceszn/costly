5bd25c505255f73e99ba6ef1b75db6253577eca7
Duruoha Ikenna <duruohaikenna@gmail.com>
2026-05-23 10:33:25 +0100

    feat: add icon SVG and update layout component for minimum height

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -7,7 +7,7 @@ export default function SearchLayout({
 }) {
   return (
     <div className="flex flex-col items-center gap-16 min-h-screen">
-      <div className="w-full flex flex-col items-center justify-end gap-12 sm:gap-18 h-[calc(100vh/1.8)] md:h-[calc(100vh/2.2)] lg:h-[calc(100vh/2.7)] ">
+      <div className="w-full flex flex-col items-center justify-end gap-12 sm:gap-18 h-[calc(100vh/1.8)] md:h-[calc(100vh/2.2)] lg:h-[calc(100vh/2.7)] min-h-[300px]">
         <h1 className="text-lg md:text-2xl">Welcome to</h1>
         <Logo />
       </div>
